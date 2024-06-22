ca341beecce16e4299c73e4929570100227b806a
andre.orvalho <andre.orvalho@gmail.com>
2024-06-22 20:48:33 +0200

    added test

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ cd backend
 python manage.py migrate backend
 python manage.py migrate zombie_invasion
 
-
 USAGE:
 #start server
 python manage.py createsuperuser
+
+visit: http://localhost:8000/admin and check if you can log in
+
+RUN TESTS:
+
+python manage.py test
--- backend/zombie_invasion/tests.py
+++ backend/zombie_invasion/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Survivor
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Survivor.objects.create(name="Ana", age=24, gender='female', latitute='123123', longitude='65464564')
+        Survivor.objects.create(name="Andre", age=24, gender='male', latitute='23434', longitude='98988')
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        ana = Survivor.objects.get(name='Ana')
+        andre = Survivor.objects.get(name='Andre')
+        self.assertEqual(ana._str_(), 'Ana')
+        self.assertEqual(andre._str_(), 'Andre')
